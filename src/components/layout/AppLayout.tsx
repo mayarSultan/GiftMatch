882f5708f6eb96aa2d170f391f767458e9190060
mayar <mayaripaad@gmail.com>
2026-07-21 23:36:37 +0300

    chore: remove stale files from earlier project structure

--- src/components/layout/AppLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom'
-
-export function AppLayout() {
-  return (
-    <div className="min-h-screen bg-background text-foreground">
-      <Outlet />
-    </div>
-  )
-}
